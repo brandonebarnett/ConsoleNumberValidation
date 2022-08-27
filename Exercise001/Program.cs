b98382dbf7d8691e2abb232e3798e186d7760d1c
Brandon <brandon.ebarnett@gmail.com>
2022-08-27 09:41:20 -0400

    Add project files.

---
+++ Exercise001/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Write a program that asks the user to enter a number between 1 and 10.
+ * If the number is valid, print Valid to the console, otherwise print Invalid. */
+
+Console.WriteLine("--------------------------------------------------------------------");
+Console.WriteLine("---------------------------Number Validation------------------------");
+Console.WriteLine("--------------------------------------------------------------------");
+Console.WriteLine();
+Console.Write("Please enter a number between 1 & 10: ");
+int userInput = Convert.ToInt32(Console.ReadLine());
+
+if (userInput >= 1 && userInput <= 10)
+    Console.WriteLine("Valid");
+else Console.WriteLine("Invalid");
